e472a748eab74c45fe53d3ea88221fe70b98b9c1
cbnolok <cbnolok@gmail.com>
2025-07-25 20:10:36 +0200

    Prepare for further log formatting (specifically, Bevy default logs, but the new feature needed hasn't yet stabilized)

--- dynamapper/src/core.rs
+++ dynamapper/src/core.rs
@@ -9,6 +9,7 @@ mod uo_files_loader;
 use crate::prelude::*;
 use bevy::{
     //ecs::schedule::ExecutorKind,
+    log::{BoxedLayer, LogPlugin},
     pbr::wireframe::{WireframeConfig, WireframePlugin},
     prelude::*,
     render::{
@@ -21,102 +22,144 @@ use bevy::{
 use bevy_framepace::FramepacePlugin;
 use std::{process::ExitCode, time::Duration};
 use system_sets::*;
-use tracing_subscriber::{EnvFilter, fmt, prelude::*};
+use tracing_subscriber::fmt;
 
-pub fn run_bevy_app() -> ExitCode {
-    // Install the custom log subscriber (must come BEFORE Bevy app launch!)
-    //  to change the default Bevy log format.
-    tracing_subscriber::registry()
-        .with(
-            fmt::layer()
-                .with_ansi(true) // colored output like Bevy default
-                .with_level(true)
-                .with_target(true)
-                // Use chrono for timestamp, format with NO milliseconds
-                .with_timer(fmt::time::ChronoLocal::new("%Y-%m-%d %H:%M:%s".into()))
-                .compact(), // Looks a lot like Bevy default (use .pretty() for multiline pretty logs)
-        )
-        .with(EnvFilter::from_default_env())
-        .init();
-
-    log_appstate_change("LoadStartupFiles");
-
-    let result = App::new()
-    .insert_resource(WinitSettings {
-        focused_mode: UpdateMode::reactive(Duration::from_secs_f64(1.0 / 60.0)),    // 60.0 Hz
-        unfocused_mode: UpdateMode::reactive_low_power(Duration::from_secs_f64(1.0 / 30.0)), // 30.0 Hz
-    })
-    .add_plugins(
-        DefaultPlugins
-        .build()
-        .disable::<bevy::log::LogPlugin>()
-        .set(ImagePlugin::default_linear())
-        .set(
-            WindowPlugin {
-                primary_window: Some(Window {
-                    title: "UODynamapper".to_string(),
-                    resizable: true,
-                    // Force 1:1 aspect for virtual rendering (game world)
-                    // UO requires 'virtual' 44×44 diamonds, so...
-                    resolution: WindowResolution::new(1320.0, 924.0), // (44*30)x(44*21), etc
-                    resize_constraints: WindowResizeConstraints {
-                        min_width: 44.0 * 10.0,
-                        min_height: 44.0 * 10.0,
-                        ..Default::default()
-                    },
-                    // Let window freely resize, but camera+scene SYSTEMS keep virtual grid and diamonds fixed.
-                    ..Default::default()
-                }),
-                ..Default::default()
-            },
-        )
-        .set(RenderPlugin {
-            render_creation: RenderCreation::Automatic(WgpuSettings {
-                features: WgpuFeatures::POLYGON_MODE_LINE, // Required for wireframe
-                ..Default::default()
-            }),
-            ..Default::default()
-        },
+#[allow(unused)]
+fn bevy_logging_custom_layer(_app: &mut App) -> Option<BoxedLayer> {
+    Some(Box::new(
+        fmt::layer()
+            .with_ansi(true) // colored output like Bevy default
+            .with_level(true)
+            .with_target(true)
+            // Use chrono for timestamp, format with NO milliseconds
+            .with_timer(fmt::time::ChronoLocal::new("%H:%M:%S".into()))
+            // compact() looks a lot like Bevy default
+            // (use .pretty() for multiline pretty logs)
+            .pretty(),
     ))
-    //.add_plugins(WireframePlugin::default()) // Needed enable wireframe rendering
+}
+
+/*
+// Work in progress for a new Bevy version?
+#[allow(unused)]
+fn bevy_logging_fmt_layer(_app: &mut App) -> Option<BoxedFmtLayer> {
+    Some(Box::new(
+        fmt::Layer::default()
+            .without_time()
+            .with_writer(std::io::stderr),
+    ))
+}
+*/
+
+fn custom_bevy_log_config() -> LogPlugin {
+    LogPlugin {
+        //custom_layer: bevy_logging_custom_layer,
+        ..Default::default()
+    }
+}
+
+fn custom_wireframe_config() -> WireframeConfig {
     // Wireframes can be configured with this resource. This can be changed at runtime.
-    .insert_resource(WireframeConfig {
+    WireframeConfig {
         // The global wireframe config enables drawing of wireframes on every mesh,
         // except those with `NoWireframe`. Meshes with `Wireframe` will always have a wireframe,
         // regardless of the global configuration.
         global: true,
         // Controls the default color of all wireframes. Used as the default color for global wireframes.
         // Can be changed per mesh using the `WireframeColor` component.
-        default_color: Color::srgb_from_array(bevy::color::palettes::css::WHITE.to_f32_array_no_alpha()),
-    })
-    /*
-    .edit_schedule(Update, |schedule| {
-    schedule.set_executor_kind(ExecutorKind::SingleThreaded);
-    })
-    */
-    .add_plugins(FramepacePlugin) // caps at 60 FPS by default
-    //.use(bevy_framepace::FramepaceSettings::default().with_framerate(30.0))
-    .add_plugins((
-        render::RenderPlugin { registered_by: "Core" },
-        texture_cache::TextureCachePlugin { registered_by: "Core" },
-        uo_files_loader::UoFilesPlugin { registered_by: "Core" },
-    ))
-    .init_state::<AppState>()
-    .insert_state(AppState::LoadStartupFiles)
-    .configure_sets(Startup, StartupSysSet::SetupScene)
-    .add_systems(
-        OnEnter(AppState::LoadStartupFiles),
-        advance_state_after_load_startup_files.after(StartupSysSet::LoadStartupFiles),
+        default_color: Color::srgb_from_array(
+            bevy::color::palettes::css::WHITE.to_f32_array_no_alpha(),
+        ),
+    }
+}
+
+fn custom_winit_settings() -> WinitSettings {
+    WinitSettings {
+        focused_mode: UpdateMode::reactive(Duration::from_secs_f64(1.0 / 60.0)), // 60.0 Hz
+        unfocused_mode: UpdateMode::reactive_low_power(Duration::from_secs_f64(1.0 / 30.0)), // 30.0 Hz
+    }
+}
+
+fn custom_window_plugin_settings() -> WindowPlugin {
+    WindowPlugin {
+        primary_window: Some(Window {
+            title: "UODynamapper".to_string(),
+            resizable: true,
+            // Force 1:1 aspect for virtual rendering (game world)
+            // UO requires 'virtual' 44×44 diamonds, so...
+            resolution: WindowResolution::new(1320.0, 924.0), // (44*30)x(44*21), etc
+            resize_constraints: WindowResizeConstraints {
+                min_width: 44.0 * 10.0,
+                min_height: 44.0 * 10.0,
+                ..Default::default()
+            },
+            // Let window freely resize, but camera+scene SYSTEMS keep virtual grid and diamonds fixed.
+            ..Default::default()
+        }),
+        ..Default::default()
+    }
+}
+
+fn custom_render_plugin_settings() -> RenderPlugin {
+    RenderPlugin {
+        render_creation: RenderCreation::Automatic(WgpuSettings {
+            features: WgpuFeatures::POLYGON_MODE_LINE, // Required for wireframe
+            ..Default::default()
+        }),
+        ..Default::default()
+    }
+}
+
+pub fn run_bevy_app() -> ExitCode {
+    // Current working directory.
+    println!("CWD: {:?}", std::env::current_dir().unwrap());
+    // Other debug info.
+    //println!("ASSET FOLDER: {:?}", bevy::asset::AssetPlugin::default().file_path);
+
+    let result = App::new()
+        .insert_resource(custom_winit_settings())
+        .add_plugins(
+            DefaultPlugins
+                .build()
+                .set(custom_bevy_log_config())
+                .set(custom_window_plugin_settings())
+                .set(custom_render_plugin_settings())
+                .set(ImagePlugin::default_linear()),
         )
-    .add_systems(
-        OnEnter(AppState::SetupSceneStage1),
-        advance_state_after_scene_setup_stage_1.after(StartupSysSet::SetupScene),
+        //.add_plugins(WireframePlugin::default()) // Needed enable wireframe rendering
+        .insert_resource(custom_wireframe_config())
+        //.edit_schedule(Update, |schedule| {
+        //  schedule.set_executor_kind(ExecutorKind::SingleThreaded);
+        //})
+        .add_plugins(FramepacePlugin) // caps at 60 FPS by default
+        //.use(bevy_framepace::FramepaceSettings::default().with_framerate(30.0))
+        .add_plugins((
+            render::RenderPlugin {
+                registered_by: "Core",
+            },
+            texture_cache::TextureCachePlugin {
+                registered_by: "Core",
+            },
+            uo_files_loader::UoFilesPlugin {
+                registered_by: "Core",
+            },
+        ))
+        .init_state::<AppState>()
+        .insert_state(AppState::LoadStartupFiles)
+        .configure_sets(Startup, StartupSysSet::SetupScene)
+        .add_systems(
+            OnEnter(AppState::LoadStartupFiles),
+            advance_state_after_load_startup_files.after(StartupSysSet::LoadStartupFiles),
         )
-    .add_systems(
-        OnEnter(AppState::SetupSceneStage2),
-        advance_state_after_scene_setup_stage_2.after(StartupSysSet::SetupScene),
+        .add_systems(
+            OnEnter(AppState::SetupSceneStage1),
+            advance_state_after_scene_setup_stage_1.after(StartupSysSet::SetupScene),
         )
-    .run();
+        .add_systems(
+            OnEnter(AppState::SetupSceneStage2),
+            advance_state_after_scene_setup_stage_2.after(StartupSysSet::SetupScene),
+        )
+        .run();
 
     match result {
         AppExit::Success => ExitCode::SUCCESS,
